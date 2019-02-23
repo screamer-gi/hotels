20ab3105045c91732b0caf8ae8d4078313293844
George Istomin <gi.screamer@gmail.com>
2019-02-23 15:14:58 +0200

    routing

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 
+use function DI\create;
+use function DI\get;
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
 use Hotel\HelloWorld;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
 use Relay\Relay;
 use Zend\Diactoros\ServerRequestFactory;
 
@@ -10,15 +16,20 @@ $containerBuilder = new \DI\ContainerBuilder();
 $containerBuilder->useAutowiring(false);
 $containerBuilder->useAnnotations(false);
 $containerBuilder->addDefinitions([
-    HelloWorld::class => \DI\create(HelloWorld::class)
+    HelloWorld::class => create(HelloWorld::class)
+        ->constructor(get('Foo')),
+    'Foo' => 'bar',
 ]);
 
 $container = $containerBuilder->build();
 
-$middlewareQueue = [];
+$routes = simpleDispatcher(function (RouteCollector $r) {
+    $r->get('/hello', HelloWorld::class);
+});
+
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler();
 
 $requestHandler = new Relay($middlewareQueue);
+//exit;
 $requestHandler->handle(ServerRequestFactory::fromGlobals());
-
-$helloWorld = $container->get(HelloWorld::class);
-$helloWorld->announce();
--- src/Hotel/HelloWorld.php
+++ src/Hotel/HelloWorld.php
@@ -4,8 +4,22 @@ namespace Hotel;
 
 class HelloWorld
 {
+    /** @var string */
+    private $foo;
+
     public function announce(): void
     {
         echo 'Hello, autoloaded world!';
+    }
+
+    public function __invoke(): void
+    {
+        echo "Hello, {$this->foo} world!";
+        exit;
+    }
+
+    public function __construct(string $foo='cool')
+    {
+        $this->foo = $foo;
     }
 }
