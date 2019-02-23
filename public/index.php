db8ae46ee13eb3ef741ed4b1b957d89a075d587b
George Istomin <gi.screamer@gmail.com>
2019-02-23 23:19:04 +0200

    bootstrap app

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use DI\ContainerBuilder;
+use FastRoute\RouteCollector;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Relay\Relay;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
+use function FastRoute\simpleDispatcher;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(true);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+]);
+
+/** @noinspection PhpUnhandledExceptionInspection */
+$container = $containerBuilder->build();
+
+$routes = simpleDispatcher(function (RouteCollector $r) {
+});
+
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler($container);
+
+$requestHandler = new Relay($middlewareQueue);
+$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
+
+$emitter = new SapiEmitter();
+return $emitter->emit($response);
