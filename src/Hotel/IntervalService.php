78ec8cace54da9113faae62abf9e512f2d0999dc
George Istomin <gi.screamer@gmail.com>
2019-02-28 15:09:41 +0200

    intersections

--- src/Hotel/IntervalService.php
+++ src/Hotel/IntervalService.php
@@ -3,6 +3,8 @@
 namespace Hotel;
 
 use Common\DbInterface;
+use DateInterval;
+use DateTimeImmutable;
 use Exception;
 use LessQL\Row;
 
@@ -11,36 +13,104 @@ class IntervalService
     /** @var DbInterface */
     private $db;
 
+    /** @var DateInterval */
+    private $dayInterval;
+
     public function __construct(DbInterface $db)
     {
         $this->db = $db;
+        $this->dayInterval = new DateInterval('P1D');
     }
 
     public function create(Row $interval): bool
     {
+        return $this->processIntersections($interval);
+    }
+
+    public function update(Row $interval): bool
+    {
+        return $this->processIntersections($interval);
+    }
+
+    protected function processIntersections(Row $interval): bool
+    {
         try {
             $this->db->begin();
+
+            $this->deleteInnerIntervals($interval);
+            $this->fixIntersections($interval);
             $interval->save($interval);
+
             $this->db->commit();
         } catch (Exception $e) {
             $this->db->rollback();
+
             return false;
         }
 
         return true;
     }
 
-    public function update(Row $interval): bool
+    private function deleteInnerIntervals(Row $interval): void
     {
-        try {
-            $this->db->begin();
-            $interval->save($interval);
-            $this->db->commit();
-        } catch (Exception $e) {
-            $this->db->rollback();
-            return false;
+        $query = $this->db->intervals()->where('date_start >= :dateStart AND date_end <= :dateEnd', [
+            'dateStart' => $interval->date_start,
+            'dateEnd' => $interval->date_end,
+        ]);
+
+        if ($interval->getId()) {
+            $query = $query->whereNot('id', $interval->getId());
         }
 
-        return true;
+        $query->delete();
+    }
+
+    private function fixIntersections(Row $interval): void
+    {
+        $query = $this->db->intervals()->where(
+            '(date_start < :dateStart AND date_end > :dateEnd) OR (date_start BETWEEN :dateStart AND :dateEnd) OR (date_end BETWEEN :dateStart AND :dateEnd)',
+            ['dateStart' => $interval->date_start, 'dateEnd' => $interval->date_end]);
+
+        if ($interval->getId()) {
+            $query = $query->whereNot('id', $interval->getId());
+        }
+
+        $outerIntervals = $query->fetchAll();
+        $interval = $this->convertDates($interval);
+
+        foreach ($outerIntervals as $outerInterval) {
+            if ($outerInterval->getId() == $interval->getId()) {
+                continue;
+            }
+
+            $convertedInterval = $this->convertDates($outerInterval);
+            if ($convertedInterval->date_start < $interval->date_start && $convertedInterval->date_end > $interval->date_end) {
+                $closingInterval = $this->db->createRow('intervals', [
+                    'date_start' => $interval->date_end->add($this->dayInterval)->format('Y-m-d'),
+                    'date_end' => $outerInterval->date_end,
+                    'price' => $outerInterval->price,
+                ]);
+                $outerInterval->date_end = $interval->date_start->sub($this->dayInterval)->format('Y-m-d');
+                $outerInterval->save();
+                $closingInterval->save();
+                continue;
+            }
+
+            if ($convertedInterval->date_start < $interval->date_start) {
+                $outerInterval->date_end = $interval->date_start->sub($this->dayInterval)->format('Y-m-d');
+            } else {
+                $outerInterval->date_start = $interval->date_end->add($this->dayInterval)->format('Y-m-d');
+            }
+            $outerInterval->save();
+        }
+    }
+
+    private function convertDates(Row $interval): Row
+    {
+        $newInterval = clone $interval;
+        $newInterval->date_start = DateTimeImmutable::createFromFormat('Y-m-d', $interval->date_start);
+        $newInterval->date_end = DateTimeImmutable::createFromFormat('Y-m-d', $interval->date_end);
+
+        return $newInterval;
     }
 }
